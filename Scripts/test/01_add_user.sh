208bd1b5b4c082b7d3c6b27db06fdf67f67ef2fc
Nicolas <otaku.prod.postmaster@gmail.com>
2022-08-06 10:10:44 +0200

    Test couleurs background

--- Scripts/test/01_add_user.sh
+++ Scripts/test/01_add_user.sh
@@ -51,6 +51,11 @@ green_text()
   printf '\E[32m'
 }
 
+blue_text() 
+{
+  printf '\E[34m'
+}
+
 red_background()
 {
   printf '\E[41m'
@@ -66,9 +71,11 @@ reset_color()
 the_question()
 {
   quit_rappel
+  blue_text
   echo "Bonjour,"
   echo "Que désirez vous faire ?"
   read -p "Ajouter un utilisateur ? 1 `echo $'\nModifier un utilisateur ? 2 '` `echo $'\nSupprimer un utilisateur ? 3 '` `echo $'\nVoir les utilisateurs existant ? 4 '` `echo $'\nVoir les groupes existant ? 5 '` `echo $'\n> '`" choix
+  reset_color
   the_choix
 }
 
