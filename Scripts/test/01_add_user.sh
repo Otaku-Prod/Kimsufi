5fd15dacd46759105bc73def4fb0feeb6e6c38d2
Nicolas <otaku.prod.postmaster@gmail.com>
2022-08-07 22:57:31 +0200

    Test gestion des couleurs

--- Scripts/test/01_add_user.sh
+++ Scripts/test/01_add_user.sh
@@ -656,7 +656,7 @@ add_group()
   usermod -a -G $otaku_script_group_id $otaku_script_login
   green_text
   echo "Le ou les groupes '$otaku_script_group_id' ont bien été ajouté à l'utilisateur $otaku_script_login."
-  id - g $otaku_script_login
+  id -g $otaku_script_login
   reset_color
 }
 
@@ -665,7 +665,7 @@ change_group()
   green_text
   usermod -g $otaku_script_group_id $otaku_script_login
   echo "Le groupe principal de l'utilisateur '$otaku_script_login' est maintenant '$otaku_script_group_id'."
-  id - g $otaku_script_login
+  id -g $otaku_script_login
   reset_color
 }
 
