472673539ad26d4882fcd96ad8db0e8afbe7d867
Nicolas <otaku.prod.postmaster@gmail.com>
2022-08-18 16:54:02 +0200

    du neuf

--- Scripts/test/install.sh
+++ Scripts/test/install.sh
@@ -10,27 +10,27 @@ otaku_prod_default_name=kimsufi
 otaku_prod_user_password=Otaku-Prod
 
 # Préparation
-apt-get -y update
-apt-get -y dist-upgrade
+apt-get -y update > /dev/null 2>&1
+apt-get -y dist-upgrade > /dev/null 2>&1
 groupadd $otaku_prod_default_name
 useradd --create-home --gid "$otaku_prod_default_name" --comment "Utilisateur Kimsufi" "$otaku_prod_default_name"
 usermod -a -G root,sudo,adm $otaku_prod_default_name
-echo -e "$otaku_prod_user_password\n$otaku_prod_user_password" | passwd $otaku_prod_default_name
-apt-get -y install perl
+echo -e "$otaku_prod_user_password\n$otaku_prod_user_password" | passwd $otaku_prod_default_name > /dev/null 2>&1
+apt-get -y install perl wget > /dev/null 2>&1
 deluser --remove-home ubuntu
 echo "gpu_mem=16" | tee -a /boot/config.txt
 
 # Docker
-apt-get remove docker docker-engine docker.io docker-ce docker-ce-cli containerd containerd.io docker-compose-plugin runc
-apt-get -y install ca-certificates curl gnupg lsb-release
+apt-get remove docker docker-engine docker.io docker-ce docker-ce-cli containerd containerd.io docker-compose-plugin runc > /dev/null 2>&1
+apt-get -y install ca-certificates curl gnupg lsb-release > /dev/null 2>&1
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null 2>&1
 chmod a+r /etc/apt/keyrings/docker.gpg
-apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get update > /dev/null 2>&1
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin > /dev/null 2>&1
 usermod -aG docker $otaku_prod_default_name
 
 # Docker-Compose
